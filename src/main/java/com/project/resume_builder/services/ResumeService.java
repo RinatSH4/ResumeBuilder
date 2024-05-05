a7052179b675c6c0f44af6ddd0d070c409027329
STALKSA <ksy9090@mail.ru>
2024-05-05 17:59:23 +0300

    added anotation

--- src/main/java/com/project/resume_builder/services/ResumeService.java
+++ src/main/java/com/project/resume_builder/services/ResumeService.java
@@ -1,7 +1,9 @@
 package com.project.resume_builder.services;
 
 import com.project.resume_builder.models.Resume;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ResumeService {
     Resume createResume(Resume resume);
     // Другие методы сервиса
--- src/main/java/com/project/resume_builder/services/UserService.java
+++ src/main/java/com/project/resume_builder/services/UserService.java
@@ -1,7 +1,9 @@
 package com.project.resume_builder.services;
 
 import com.project.resume_builder.models.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
     User registerUser(User user);
     // Другие методы сервиса
